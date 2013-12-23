1dd0c7388e7e8ad87b08f128971d7225c2775c85
Paul Dornfeld <pdornfel@gmail.com>
2013-12-23 00:15:46 -0500

    added ability to enter title and description

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,4 +4,21 @@ class EntriesController < ActionController::Base
     @entries = Entry.all
   end
 
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = Entry.new(event_params)
+
+    respond_to do |format|
+      @entry.save
+      format.html { redirect_to :entries }
+    end
+  end
+
+  def event_params
+      params.require(:entry).permit(:title, :description)
+  end
+
 end
